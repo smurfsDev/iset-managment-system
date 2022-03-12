6f580eeca2aaf29e14f8f2330df89b994063031c
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-12 01:14:33 +0100

    [Laravel:routes] add vue routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/{any?}',function(){
+    return view('vue');
+});
