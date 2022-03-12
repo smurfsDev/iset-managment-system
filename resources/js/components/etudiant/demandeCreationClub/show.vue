8340c7bf0fc0eb5ee61b8467bfc70990908d4475
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-12 10:59:16 +0100

    [Vue] complete demandeCreationClub show component

--- resources/js/components/etudiant/demandeCreationClub/show.vue
+++ resources/js/components/etudiant/demandeCreationClub/show.vue
@@ -1,76 +1,9 @@
 <template>
-    <div>
+    <div >
         <div class="card card-body my-5 py-5 text-center" v-if="demandes.length == 0">
-            <h3>il y'a aucun demande</h3>
+            <h3>il y'a aucune demande</h3>
         </div>
-        <!-- <main>
-            <data-table v-bind="parameters" @actionTriggered="handleAction" />
-        </main>-->
-        <!-- <div
-            class="modal fade"
-            id="ficheModal"
-            tabindex="-1"
-            aria-labelledby="demandeModalLabel"
-            aria-hidden="true"
-        >
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="demandeModalLabel">Les fiches</h5>
-                        <button
-                            type="button"
-                            class="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group mb-2">
-                            <b-row class="jutify-content-center mb-2">
-                                <b-col v-if="fiches.length == 0" cols="10">Aucune fiche de paie</b-col>
-                            </b-row>
-                            <b-row
-                                v-show="fiches.length != 0"
-                                v-for="fiche in fiches"
-                                :key="fiche.id"
-                                class="justify-content-center mb-2"
-                            >
-                                <b-col cols="10">
-                                    <b-row>
-                                        <b-col>date : {{ fiche.date }}</b-col>
-                                    </b-row>
-                                    <b-row>
-                                        <b-col>
-                                            <h6>
-                                                <b>Rebriques:</b>
-                                            </h6>
-                                        </b-col>
-                                    </b-row>
-                                    <b-row>
-                                        <b-col cols="9">total : {{ fiche.total }}</b-col>
-                                        <b-col cols="3">
-                                            <b-button
-                                                variant="success"
-                                                v-on:click="getPdf(id, fiche.id)"
-                                            >Afficher</b-button>
-                                        </b-col>
-                                    </b-row>
-                                </b-col>
-                                <hr />
-                            </b-row>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button
-                            type="button"
-                            class="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                        >Fermer</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        <b-card v-for="demande in demandes" :key="demande.id">
+        <b-card class="my-2" v-for="demande in demandes" :key="demande.id">
             <md-tabs>
                 <md-tab id="tab-home" md-label="demande">
                     <b-container class="bv-example-row text-center">
@@ -94,7 +27,7 @@
                 </md-tab>
             </md-tabs>
         </b-card>
-        <nav class="row">
+        <nav class="row" v-if="demandes.length!=0">
             <ul class="pagination w-auto mx-auto">
                 <li :class="[{ disabled: !pagination.prev_page_url }]" class="page-item">
                     <a
