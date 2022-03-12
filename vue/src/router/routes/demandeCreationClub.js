6af2bcbf14ba85ae61487f69725fc69c1835b9ed
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-13 00:45:47 +0100

    [Vue:Routes] Create demandeCreationClub routes

---
+++ vue/src/router/routes/demandeCreationClub.js
@@ -0,0 +1,12 @@
+const demandeCreationClub = () =>
+  import(
+    "../../components/etudiant/demandeCreationClub/demandeCreationClub.vue"
+  );
+
+export default [
+  {
+    path: "/demandeCreationClub",
+    component: demandeCreationClub,
+    name: "demandeCreationClub",
+  },
+];
