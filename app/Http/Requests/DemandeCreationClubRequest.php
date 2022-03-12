cfd75312e10be4beb668abc301063dd3f809fe43
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-12 01:13:15 +0100

    [Laravel:model] creation DemandeCreationClub request

---
+++ app/Http/Requests/DemandeCreationClubRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class DemandeCreationClubRequest extends FormRequest
+{
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "nomClub"=> "required|unique:demandeCreationClub",
+            "dateCreation"=> "required|date",
+            "activite"=> "required",
+            "president"=> "required|min:2",
+            "vicePresident"=> "required|min:2",
+            "responsableClubId" => "required|exists:users,id"
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
