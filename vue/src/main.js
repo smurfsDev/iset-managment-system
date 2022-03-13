e4d31c26124c9ba3dfe6facb70e05cdae8ca1fc7
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-13 22:18:38 +0100

    [Vue] add global methods, order imports to fix ui bugs

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,22 +1,26 @@
+import "mdb-vue-ui-kit/css/mdb.min.css";
 import Vue from "vue";
-import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 let Bootstrap = require("bootstrap");
 Vue.use(Bootstrap);
-import "bootstrap";
-import 'bootstrap/dist/js/bootstrap.min.js'
+import App from "./App.vue";
+// Vue.use(Bootstrap);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 import Vuelidate from "vuelidate";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "vue-material/dist/vue-material.min.css";
-import 'mdbvue/lib/css/mdb.min.css'
-// import "mdb-vue-ui-kit/css/mdb.min.css";
+import "mdbvue/lib/css/mdb.min.css";
 import Vuetify from "vuetify";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import "@andresouzaabreu/vue-data-table/dist/DataTable.css";
 import VueScreen from "vue-screen";
 import VueMaterial from "vue-material";
@@ -24,8 +28,6 @@ Vue.use(VueMaterial);
 const vuetify = new Vuetify();
 Vue.use(vuetify);
 Vue.use(VueSidebarMenu);
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 Vue.use(Vuelidate);
 Vue.use(VueScreen);
 window.Vue = require("vue");
@@ -33,6 +35,17 @@ window.router = router;
 window.Fire = new Vue();
 import "boxicons";
 Vue.config.productionTip = false;
+
+Vue.mixin({
+  methods: {
+    showModal: function (id) {
+      $(`#${id}`).modal("show");
+    },
+    hideModal: function (id) {
+      $(`#${id}`).modal("hide");
+    },
+  },
+});
 
 new Vue({
   router,
