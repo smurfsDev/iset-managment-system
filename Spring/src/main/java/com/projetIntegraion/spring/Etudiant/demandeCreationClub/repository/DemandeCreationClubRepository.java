c801c259e742effba74b7a6d23f525336fb6e3f1
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-17 14:12:10 +0100

    [Spring] Add responsableClubId filtering

--- Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeCreationClubRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeCreationClubRepository.java
@@ -1,8 +1,12 @@
 package com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository;
+
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.DemandeCreationClub;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public interface DemandeCreationClubRepository extends JpaRepository<DemandeCreationClub, Long>{
+    Page<DemandeCreationClub> findByresponsableClubId(Long id,Pageable pageable);
 }
