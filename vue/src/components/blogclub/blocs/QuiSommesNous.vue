28a8c11e6fefffe40ad18b28e0994efaf72d3c6b
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-22 21:31:41 +0100

    [Vue] update QuiSommesNous component

--- vue/src/components/blogclub/blocs/QuiSommesNous.vue
+++ vue/src/components/blogclub/blocs/QuiSommesNous.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
          <article class="blog-post">
-            <h2 class="blog-post-title">Qui sommes-nous ?</h2>
-            <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
-
+            <h2 class="blog-post-title">Qui sommes-nous ?(hedhii description détaillée lel club)</h2>
+           
             <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
        
             <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
