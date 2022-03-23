615337815faf086a5e7ed6a0a6dd04180e7c96ee
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-23 20:07:12 +0100

    [Vue] Update projets component

--- vue/src/components/blogclub/blocs/projets.vue
+++ vue/src/components/blogclub/blocs/projets.vue
@@ -2,7 +2,7 @@
 <article>
   
 <div class="blog-post" id="custom-cards">
-    <h2 class="pb-2 border-bottom">Custom cards</h2>
+    <h2 class="pb-2 border-bottom">Nos projets</h2>
 
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
       <div class="col">
