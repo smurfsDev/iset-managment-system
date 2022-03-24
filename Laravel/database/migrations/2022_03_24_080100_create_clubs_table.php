4e6f175d06388f64d11f3814e31b5ad8c69bebe7
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-24 09:18:52 +0100

    [Laravel] Create clubs migration and model

---
+++ Laravel/database/migrations/2022_03_24_080100_create_clubs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('nom');
+            $table->date('dateCreation');
+            $table->string('responsableClub');
+            $table->string('president');
+            $table->string('vicePresident'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
