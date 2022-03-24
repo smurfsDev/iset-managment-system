c3e7cf1f45c006036dcd274ee0c1e36e2e52464e
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-24 09:39:46 +0100

    [Laravel] create abouts migration and model

---
+++ Laravel/app/Models/about.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class about extends Model
+{
+    use HasFactory;
+    protected $table = 'about';
+    protected $fillable = [
+        "longDescription",
+        "idClub"
+    ];
+}
