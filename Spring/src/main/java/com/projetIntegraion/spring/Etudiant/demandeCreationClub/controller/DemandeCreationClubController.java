8c4d0e459b9dbfa9eee4603d662967c297d63ce5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-13 18:07:13 +0100

    [Spring] Create ShowList method

--- Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/controller/DemandeCreationClubController.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/controller/DemandeCreationClubController.java
@@ -1,89 +1,34 @@
-// TODO: Fix responses 
-// TODO: Fix api call uri 
 package com.projetIntegraion.spring.Etudiant.demandeCreationClub.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import java.text.ParseException;
-// import java.util.List;
-
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.DemandeCreationClub;
-import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.ResponsePagination;
-import com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository.DemandeCreationClubRepository;
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.service.DemandeCreationClubService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin(origins = "http://localhost:8080")
-@RestController
+@Controller
 public class DemandeCreationClubController {
+    @Autowired
+    private DemandeCreationClubService DemandeCreationClubService;
 
-	@Autowired
-	DemandeCreationClubService DemandeCreationClubService;
-	@Autowired
-	DemandeCreationClubRepository dccr;
 
-	@RequestMapping(value = "/dcc/{id}", method = RequestMethod.GET)
-	public ResponsePagination employeesPageable(
-		@PathVariable(name = "id", required = false) String id,
-		Pageable pageable) {
-		Page <DemandeCreationClub> d = dccr.findByresponsableClubId(Long.parseLong(id),pageable);
-		ResponsePagination res = new ResponsePagination();
-		res.setData(d.getContent());
-		res.setCurrent_page(d.getPageable().getPageNumber()+1);
-		res.setLast_page(d.getTotalPages());
-		res.setNext_page_url(d.hasNext()?"http://localhost:8000/api/dcc/"+id+"?page="+d.getPageable().next().getPageNumber()+"":null);
-		res.setPrev_page_url(d.hasPrevious()?"http://localhost:8000/api/dcc/"+id+"?page="+d.getPageable().previousOrFirst().getPageNumber()+"":null);
-		return res;
-	}
 
-	@PostMapping("/dcc")
-	public DemandeCreationClub save(@RequestBody DemandeCreationClub DemandeCreationClub)
-			throws ParseException {
-		DemandeCreationClub dcc = new DemandeCreationClub();
-		dcc.setNomClub(DemandeCreationClub.getNomClub());
-		dcc.setActivite(DemandeCreationClub.getActivite());
-		dcc.setSujet(DemandeCreationClub.getSujet());
-		dcc.setLogo(DemandeCreationClub.getLogo());
-		dcc.setDateCreation(DemandeCreationClub.getDateCreation());
-		dcc.setPresident(DemandeCreationClub.getPresident());
-		dcc.setVicePresident(DemandeCreationClub.getVicePresident());
-		dcc.setResponsableClubId(DemandeCreationClub.getResponsableClubId());
-		DemandeCreationClub saveProduit = DemandeCreationClubService.saveDemandeCreationClub(dcc);
-		return saveProduit;
-	}
-	//
-	// @GetMapping("/dcc")
-	// public ResponseEntity<List<DemandeCreationClub>> getAll() {
-	// List<DemandeCreationClub> prods =
-	// DemandeCreationClubService.getAllDemandeCreationClub();
-	// return prods.size() != 0 ? ResponseEntity.ok().body(prods) :
-	// ResponseEntity.notFound().build();
-	// }
 
-	@DeleteMapping("/dcc/{id}")
-	public ResponseEntity<String> supprimerDemandeCreationClub(@PathVariable(name = "id", required = false) String id) {
-		return DemandeCreationClubService.deleteDemandeCreationClub(Long.parseLong(id))
-				? ResponseEntity.ok().body("Produit deleted")
-				: ResponseEntity.notFound().build();
-	}
+    @RequestMapping("/listeDcc")
+    public String showList(ModelMap modelMap,
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "2") int size) {
+        Page<DemandeCreationClub> listDcc = DemandeCreationClubService.getAllDemandeCreationClubParPage(page, size);
+        modelMap.addAttribute("dccs", listDcc);
+        modelMap.addAttribute("pages", new int[listDcc.getTotalPages()]);
+		modelMap.addAttribute("currentPage", page);
 
-	@PutMapping("/dcc/{id}")
-	public ResponseEntity<DemandeCreationClub> editerDemandeCreationClubService(
-			@PathVariable(name = "id", required = false) String id,
-			@RequestBody DemandeCreationClub DemandeCreationClub) {
-		DemandeCreationClub p = DemandeCreationClubService.updateDemandeCreationClub(Long.parseLong(id), DemandeCreationClub);
-		return p != null ? ResponseEntity.ok().body(p) : ResponseEntity.notFound().build();
-	}
+        return "list";
+    }
+
+
 }
