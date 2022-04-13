6bc1c7440afc4eda2e83eeb2f743f2beced3b614
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-13 15:41:31 +0100

    [Vue] Show dcc logo

--- vue/src/components/admin/demandeCreationClub/show.vue
+++ vue/src/components/admin/demandeCreationClub/show.vue
@@ -43,8 +43,8 @@
         scale="2"
         variant="warning"
       ></b-icon>
-      <md-tabs>
-        <md-tab id="tab-home" md-label="demande">
+      <md-tabs style="height: auto;!important">
+        <md-tab  style="height: auto;!important" id="tab-home" md-label="demande">
           <div class="bv-example-row text-center">
             <b-row class="mb-2">
               <b-row>
@@ -61,7 +61,7 @@
                   President : {{ demande.president }} Vice president :
                   {{ demande.vicePresident }}
                   <br>
-                  <img :src="demande.logo" alt="logo" width="50" />
+                  <img :src="demande.logo" alt="logo" width="250" />
                 </b-col>
               </b-row>
             </b-row>
