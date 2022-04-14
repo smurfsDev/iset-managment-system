a12559ee6a98bcdea8272db8ab3ea8f5b0f3e130
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-14 21:42:58 +0100

    [Laravel] fix clubSeeder name

--- Laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         $this->call(DemandeCreationClubSeeder::class);
-        $this->call(ClubSeeder::class);
+        $this->call(clubSeeder::class);
         $this->call(MemberSeeder::class);
 
     }
