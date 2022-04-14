6703acbdf78f96fe5f88c74391e35e95821b1199
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-14 10:30:23 +0100

    [Spring] Create securityConfig class

---
+++ Spring/src/main/java/com/projetIntegraion/spring/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.projetIntegraion.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("123")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("youssef").password(passwordEncoder.encode("admin")).roles("AGENT",
+                "USER");
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("youssef")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().defaultSuccessUrl("/ListeProduits", true);
+        http.authorizeRequests().antMatchers("/showCreate", "/saveProduit").hasAnyRole("ADMIN", "AGENT");
+        http.authorizeRequests().antMatchers("/ListeProduits")
+                .hasAnyRole("ADMIN", "AGENT", "USER");
+        http.authorizeRequests()
+                .antMatchers("/supprimerProduit", "/modifierProduit", "/updateProduit")
+                .hasAnyRole("ADMIN");
+        http.exceptionHandling().accessDeniedPage("/accessDenied");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+    }
+}
