a3d70beeeb98cdab5f9cd6a42433564753c9edf5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-15 00:32:07 +0100

    [Vue] RollBack

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,7 +1,6 @@
 const webpack = require("webpack");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/YOUR_REPO_NAME/' : '/',
   // The configure Web pack is the place in Vue CLI 3.0 to configure the parameters of the webpack plug-in. If you set it here, it will create or override the default configuration of the webpack.
   // The meaning of webpack ProvidePlugin is to create a global variable that can be used within each module of webpack. The configuration meaning here is to create three variables'$','jQuery','window. jQuery'pointing to jQuery dependency and'Popper' pointing to popper. JS dependency.
   configureWebpack: {
