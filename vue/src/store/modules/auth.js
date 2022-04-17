fb04870ca6f55aebad8ee48288f90e83478bda09
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 17:51:55 +0100

    [Vue] Create login action

--- vue/src/store/modules/auth.js
+++ vue/src/store/modules/auth.js
@@ -17,7 +17,31 @@ const getters = {
   StateUser: (state) => state.user
 };
 const actions = {
- 
+  async LogIn({ commit }, User) {
+    axios
+      .get("http://localhost:8000/sanctum/csrf-cookie", {
+        withCredentials: true
+      })
+      .then(() => {})
+      .catch(() => {});
+    await axios
+      .post("http://localhost:8000/api/login", User, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          this.tkn = response.data.data.token;
+          localStorage.setItem("token", this.tkn);
+          commit("setUser", response.data.data.user);
+          commit("setToken", response.data.data.token);
+        } else {
+          console.log("jawna behi nai");
+        }
+      });
+  }
 };
 const mutations = {
   setUser(state, user) {
