de60c635d049afdf98869c8528960efb4034bba2
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 17:24:04 +0100

    [Laravel] extends BaseController

--- Laravel/app/Http/Controllers/API/AuthController.php
+++ Laravel/app/Http/Controllers/API/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
     public function signin(Request $request)
     {
