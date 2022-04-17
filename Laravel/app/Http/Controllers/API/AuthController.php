8358769b5ec4839c50f993e5c702e4d1675b680d
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 17:50:48 +0100

    [Laravel] Return auth user

--- Laravel/app/Http/Controllers/API/AuthController.php
+++ Laravel/app/Http/Controllers/API/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends BaseController
             $authUser = Auth::user();
             $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken;
             $success['name'] =  $authUser->name;
+            $success['user'] = $authUser;
 
             return $this->sendResponse($success, 'User signed in');
         } else {
