5a664ce04fda98d206fb9e0309059695dc3fb936
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 17:54:22 +0100

    [Vue] login

--- vue/src/components/auth/login.vue
+++ vue/src/components/auth/login.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="login-form">
-        <form @submit.prevent="login">
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" v-model="form.email" placeholder="Email">
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" v-model="form.password" placeholder="Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
+        <form @submit.prevent="logIn">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" v-model="form.email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" v-model="form.password"
+                    placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
     </div>
 </template>
 
 <script>
+// import axios from "axios";
+import { mapActions } from "vuex";
+
 export default {
     data() {
         return {
@@ -25,13 +29,20 @@ export default {
         }
     },
     methods: {
-        login() {
-        }
+        ...mapActions(["LogIn"]),
+         logIn() {
+            this.LogIn(this.form);
+            try {
+                 this.LogIn(this.form)
+            } catch (error) {
+                this.showError = true
+            }
+
+        },
     }
 
 }
 </script>
 
 <style>
-
 </style>
