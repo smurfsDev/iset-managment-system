98723ab508f8ca7d5580f2f5a3265d0f5beae438
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 17:51:41 +0100

    [Vue] Set user states/getters/mutations

--- vue/src/store/modules/auth.js
+++ vue/src/store/modules/auth.js
@@ -2,18 +2,30 @@ import axios from "axios";
 // import store from "..";
 
 const state = {
-
+  user: null,
+  token: null,
+  secrets: null,
+  isAuthenticated: false,
+  isAdmin: false,
+  isUser: false,
+  isSuperAdmin: false,
+  message: null
 };
 
 const getters = {
-
+  isAuthenticated: (state) => state.user !== null,
+  StateUser: (state) => state.user
 };
 const actions = {
-
+ 
 };
 const mutations = {
-  
-  
+  setUser(state, user) {
+    state.user = user;
+  },
+  setToken(state, token) {
+    state.token = token;
+  },
 };
 export default {
   state,
