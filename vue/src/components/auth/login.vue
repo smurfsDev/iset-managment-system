147a116430187ab814e1878b1fa54f8fee0d6ff5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 16:50:36 +0100

    [Vue] Create login component

---
+++ vue/src/components/auth/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login-form">
+        <form @submit.prevent="login">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" v-model="form.email" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" v-model="form.password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        login() {
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
