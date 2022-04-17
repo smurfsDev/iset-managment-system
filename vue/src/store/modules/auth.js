032ea19e6f627f256f8ac5c2d17dec12a2fbfb1f
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-17 16:45:55 +0100

    [Vue] Create auth module

---
+++ vue/src/store/modules/auth.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+// import store from "..";
+
+const state = {
+
+};
+
+const getters = {
+
+};
+const actions = {
+
+};
+const mutations = {
+  
+  
+};
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
