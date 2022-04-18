e508286d486b325d7686dbc744ea4da9b829e07f
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-18 01:17:07 +0100

    [Vue] Add register action

--- vue/src/store/modules/auth.js
+++ vue/src/store/modules/auth.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 // import store from "..";
-
+import router from "../../router/index";
 const state = {
   user: null,
   token: null,
   secrets: null,
   isAuthenticated: false,
   isAdmin: false,
-  isUser: false,
+  isStudent: false,
   isSuperAdmin: false,
   message: null
 };
 
 const getters = {
   isAuthenticated: (state) => state.user !== null,
-  StateUser: (state) => state.user
+  StateUser: (state) => state.user,
+  
+
 };
 const actions = {
   async LogIn({ commit }, User) {
@@ -40,13 +42,34 @@ const actions = {
         } else {
           console.log("jawna behi nai");
         }
+        router.push("/");
       });
   },
   async LogOut({ commit }) {
     localStorage.removeItem("token");
     commit("setUser", null);
     commit("setToken", null);
+    router.push("/login");
 
+  },
+  async Register({ commit }, User) {
+    await axios
+      .post("http://localhost:8000/api/register", User, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          this.tkn = response.data.data.token;
+          localStorage.setItem("token", this.tkn);
+          commit("setUser", response.data.data.user);
+          commit("setToken", response.data.data.token);
+        } else {
+          console.log("jawna behi nai");
+        }
+      });
   }
 };
 const mutations = {
