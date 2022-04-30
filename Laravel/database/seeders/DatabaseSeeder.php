2273677ea18873b225a57dd64af9542cfec4ce89
raed charrad <raed.charrad91@gmail.com>
2022-05-01 00:39:16 +0100

    [Laravel] fix conflict

--- Laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/database/seeders/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(1)->create();
         $this->call(DemandeCreationClubSeeder::class);
+        $this->call(clubSeeder::class);
+        $this->call(MemberSeeder::class);
+        $this->call(RolesSeeder::class);
+        DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [1, 1]);
+        DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [2, 2]);
         $this->call(CategoryMaterielsSeeder::class);
         $this->call(MaterielsSeeder::class);
         $this->call(DemandeMaterielsSeeder::class);
