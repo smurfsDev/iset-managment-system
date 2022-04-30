306de26d5299b1baf4a8faf64be8efbdf61140c9
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-30 19:40:39 +0100

    [Laravel] Create club controller

---
+++ Laravel/app/Http/Controllers/ClubController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DemandeAdhesionClubRequest;
+use App\Models\club;
+use App\Models\DemandeAdhesionClub;
+use Illuminate\Http\Request;
+
+class ClubController extends Controller
+{
+
+    public function show(){
+        $Clubs = club::paginate(5);
+        if (empty($Clubs)) {
+            return response()->json(['message' => 'No clubs found'], 404);
+        }else{
+            return response()->json($Clubs, 200);
+        }
+    }
+
+
+    public function DemandeeAdhesion(DemandeAdhesionClubRequest $request){
+        $user_id = $request->user_id;
+        $club_id = $request->input('club_id');
+        $message = $request->input('message');
+        $status = 0;
+        $demande = new DemandeAdhesionClub();
+        $demande->user_id = $user_id;
+        $demande->club_id = $club_id;
+        $demande->message = $message;
+        $demande->status = $status;
+        $demande->save();
+        return response()->json(['success'=>'Demande envoyée avec succès']);
+    }
+
+    public function getDemandeAdhesion(Request $request){
+        $demandes = $request->user()->DemandeAdhesionClub()->paginate(5);
+        if (empty($demandes)) {
+            return response()->json(['message' => 'No demande found'], 404);
+        }else{
+            return response()->json($demandes, 200);
+        }
+    }
+}
