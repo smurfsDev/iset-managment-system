1cb351afd60e9422ddc028ee02bc28f989e5aaa7
raed charrad <raed.charrad91@gmail.com>
2022-05-01 00:24:07 +0100

    [ Laravel ] make factories

---
+++ Laravel/database/factories/DemandeSalleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Salle;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DemandeSalleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "idResponsable" => User::get('id')->random(),
+            "idSalle" => Salle::get('id')->random(),
+            "dateEmploi" => $this->faker->date(),
+            "dateDeRemise" => $this->faker->date(),
+
+        ];
+    }
+}
