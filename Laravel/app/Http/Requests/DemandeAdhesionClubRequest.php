9f0a4bfd8983270d9024b21430721e3c5e23be93
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-30 12:20:10 +0100

    [Laravel] createe DemandeAdhesionClubRequest

---
+++ Laravel/app/Http/Requests/DemandeAdhesionClubRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class DemandeAdhesionClubRequest extends FormRequest
+{
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "club_id"=> "required|exists:clubs,id",
+            "user_id"=> "required|exists:users,id",
+            "message"=> "required|min:2"
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
