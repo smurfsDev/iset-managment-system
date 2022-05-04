94c03520b218e590b1598306dc6512a8525f0387
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-05-04 22:35:04 +0200

    [Spring] Create ProjectService

---
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/blogClub/service/ProjectService.java
@@ -0,0 +1,53 @@
+package com.projetIntegraion.spring.Etudiant.blogClub.service;
+
+import com.projetIntegraion.spring.Etudiant.blogClub.entity.Project;
+import com.projetIntegraion.spring.Etudiant.blogClub.repository.ProjectRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public class ProjectService {
+    @Autowired
+    private ProjectRepository ProjectRepository;
+
+    public Project save(Project p) {
+		return ProjectRepository.save(p);
+	}
+
+	public Boolean existsById(Long id) {
+		return ProjectRepository.existsById(id);
+	}
+
+	public Project updateClub(Long id, Project p) {
+		return null;
+	}
+
+	public void delete(Project p) {
+		ProjectRepository.delete(p);
+	}
+
+	public Boolean deleteById(Long id) {
+		if (ProjectRepository.existsById(id)) {
+			ProjectRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	public Project getClub(Long id) {
+		return ProjectRepository.existsById(id) == false ? null : ProjectRepository.findById(id).get();
+	}
+
+	// public List<Project> getAllClubs() {
+	// 	return ProjectRepository.findAll();
+	// }
+
+	public Page<Project> getAllDemandeCreationClubParPage(int page, int size){
+		return ProjectRepository.findAll(PageRequest.of(page, size));
+	};
+
+	// public Page<Project> getClubParNom(String nom, int page, int size){
+	// 	return ProjectRepository.findByNomClubContains(nom, page, size); //(nom, PageRequest.of(page, size));
+	// };
+}
