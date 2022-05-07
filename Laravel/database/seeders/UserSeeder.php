8aa5097f4a57b645bd9421181991b3c2703775ad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-07 06:07:59 +0100

    [laravel] fix user seeder

--- Laravel/database/seeders/UserSeeder.php
+++ Laravel/database/seeders/UserSeeder.php
@@ -67,7 +67,7 @@ class UserSeeder extends Seeder
         DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [3, 3]);
         DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [4, 2]);
         DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [5, 4]);
-        DB::insert('insert into role_user (user_id, role_id,department,status) values (?,?, ?,?)', [5, 4,1,1]);
+        DB::insert('insert into role_user (user_id, role_id,department,status) values (?,?, ?,?)', [6, 5,1,1]);
 
 
     }
