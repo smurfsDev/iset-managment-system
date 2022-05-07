ce30eb3e08ebc3043efd6a308ffde46febb1c0e8
raed charrad <raed.charrad91@gmail.com>
2022-05-07 03:12:46 +0100

    [Laravel] fix seeder

--- Laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
