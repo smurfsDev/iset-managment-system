b50e574158d9f0b1fa9fcb7092ea3e205ee5aa5a
raed charrad <raed.charrad91@gmail.com>
2022-05-09 22:06:49 +0100

    [Spring] add DepartementService

--- Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/service/DepartementService.java
+++ Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/service/DepartementService.java
@@ -1,5 +1,49 @@
 package com.projetIntegraion.spring.administrateur.departement.service;
 
+import java.util.List;
+
+import com.projetIntegraion.spring.administrateur.departement.entity.Departement;
+import com.projetIntegraion.spring.administrateur.departement.repository.DepartementRepository;
+import com.projetIntegraion.spring.demandeSalle.entity.Salle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepartementService {
+
+    @Autowired DepartementRepository departementRepository;
+    public Departement save(Departement departement){
+        return departementRepository.save(departement);
+    }
+    public boolean existsById(Long id){
+        return departementRepository.existsById(id);
+    }
+    public void delete(Departement departement){
+        departementRepository.delete(departement);
+    }
+    public Departement deleteById(Long id){
+        if (departementRepository.existsById(id)) {
+            departementRepository.deleteById(id);
+        }
+        return null;
+    }
+    public Departement getDepartementById(Long id){
+        return departementRepository.existsById(id) ? departementRepository.findById(id).get() : null;
+    }
+    public List<Departement> getAllDepartement(){
+        return departementRepository.findAll();
+    }
+    public Page<Departement> getAllDepartementParPage(int page , int size){
+        return departementRepository.findAll(PageRequest.of(page, size));
+    }
+    public List<Salle> getAllSalleByDepartement(Long id){
+        return departementRepository.findById(id).get().getSalle();
+    }
+
+    
+
 
 }
