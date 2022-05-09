09131de321be94d0c8e517110028c80e613a7df1
raed charrad <raed.charrad91@gmail.com>
2022-05-09 22:11:56 +0100

    [Spring] add SalleRepository

--- Spring/src/main/java/com/projetIntegraion/spring/demandeSalle/repository/SalleRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandeSalle/repository/SalleRepository.java
@@ -1,5 +1,11 @@
 package com.projetIntegraion.spring.demandeSalle.repository;
 
-public class SalleRepository {
+import com.projetIntegraion.spring.demandeSalle.entity.Salle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+public interface SalleRepository extends JpaRepository<Salle, Long>{
 
 }
