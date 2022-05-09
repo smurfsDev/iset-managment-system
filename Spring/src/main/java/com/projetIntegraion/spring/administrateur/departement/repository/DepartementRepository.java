59176fa5cd5129f2dead830a58efe10ce3eb26a3
raed charrad <raed.charrad91@gmail.com>
2022-05-09 22:06:28 +0100

    [Spring] add DepartementRepository

--- Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/repository/DepartementRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/repository/DepartementRepository.java
@@ -1,5 +1,12 @@
 package com.projetIntegraion.spring.administrateur.departement.repository;
 
-public class DepartementRepository {
+import com.projetIntegraion.spring.administrateur.departement.entity.Departement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+
+public interface DepartementRepository extends JpaRepository<Departement, Long> {
 
 }
