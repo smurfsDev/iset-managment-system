2509bb03af3a57ff3e2d15949a32ea2afea936ad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-19 18:45:20 +0100

    [Laravel] Create CategorieMateriel controller

--- Laravel/app/Http/Controllers/CategorieMaterielController.php
+++ Laravel/app/Http/Controllers/CategorieMaterielController.php
@@ -2,20 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategorieMatrielRequest;
 use Illuminate\Http\Request;
 use App\Models\CategorieMateriel;
 
 class CategorieMaterielController extends Controller
 {
-    public function show(){
+    public function show()
+    {
         $Categorie = CategorieMateriel::get();
-        if(sizeof($Categorie)>0){
-        return response()->json(["data"=>$Categorie], 200);
-        }
-        else
+        if (sizeof($Categorie) > 0) {
+            return response()->json(["data" => $Categorie], 200);
+        } else
             return response()->json([
                 "aucune Categorie"
             ], 404);
     }
 
+    public function store(CategorieMatrielRequest $request)
+    {
+        $Categorie = CategorieMateriel::create(
+            [
+                "titre" => $request->titre,
+            ]
+        );
+        return response()->json(["data" => $Categorie], 201);
+    }
+
+    public function update(CategorieMatrielRequest $request, $id)
+    {
+        $Categorie = CategorieMateriel::find($id);
+        if (!$Categorie) {
+            return response()->json([
+                "message" => "Categorie not found"
+            ], 404);
+        }
+        $Categorie->update(
+            [
+                "titre" => $request->titre,
+            ]
+        );
+        return response()->json(["data" => $Categorie], 200);
+    }
+
+    public function destroy($id)
+    {
+        $Categorie = CategorieMateriel::find($id);
+        if (!$Categorie) {
+            return response()->json([
+                'type' => 'DemandeCreationClub',
+                'message' => 'demande non trouvée'
+            ], 404);
+        }
+        $Categorie->delete();
+        return response()->json([], 204);
+    }
 }
