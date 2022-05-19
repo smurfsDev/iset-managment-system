fed6e20f6108b706fb8517ef70cb75ab6fae3dbb
raed charrad <raed.charrad91@gmail.com>
2022-05-19 22:16:05 +0100

    [Vue] add sendMail function to mail.vue

--- vue/src/components/club/members/mail.vue
+++ vue/src/components/club/members/mail.vue
@@ -19,16 +19,16 @@
           ></button>
         </div>
 
-        <form class="mb-3" @submit.prevent="addDemande">
+        <form class="mb-3" @submit.prevent="send">
           <div class="modal-body">
             <div class="form-group mb-2">
                 {{use}}
-                <label>Objet :</label>
+                <label>Sujet :</label>
               <div class="form-group">
-                <input type="text" class="form-control" placeholder="Objet"  required="required" />
+                <input type="text" class="form-control" v-model="sujet" placeholder="sujet"  required="required" />
               </div>
                 <label>Message :</label>
-                <textarea  class="form-control" placeholder="Message"  required="required" />
+                <textarea  class="form-control" v-model="message" placeholder="Message"  required="required" />
             </div>
           </div>
           <div class="modal-footer">
@@ -55,15 +55,29 @@ export default {
     },
     data() {
     return {
-       
+        sujet: '',
+        message: '',
+
     }
     },
-  
-  mounted() {},
-  created() {
-    },
   methods: {
-      
+        sendMail(id){
+            this.$http.post("http://127.0.0.1:8000/api/members/mail/"+id,{
+                sujet:this.sujet,
+                message:this.message,
+            }).then(res => {
+                this.hideModal('MailModal');
+                this.alert.variant = "success";
+                this.alert.msg = "Mail envoyée avec succès";
+                this.alert.dismissCountDown = 5;
+            });
+        },
+        send(){
+            for(var i=0; i < this.use.length;i++){
+
+                this.sendMail(this.use[i]);
+            }
+        }
   },
 };
 </script>
