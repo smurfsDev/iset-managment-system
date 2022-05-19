50174a0087600a55c9d4a96ed5db6a1914eea6e5
raed charrad <raed.charrad91@gmail.com>
2022-05-19 21:48:03 +0100

    [Laravel] create mail blade

---
+++ Laravel/resources/views/mail.blade.php
@@ -0,0 +1,8 @@
+<h2>Hello</h2> <br><br>
+You have got an email from : {{ $name }} <br><br>
+User details: <br><br>
+Name: {{ $name }} <br>
+Email: {{ $email }} <br>
+Subject: {{ $sujet }} <br>
+Message: {{ $user_query }} <br><br>
+Thanks
