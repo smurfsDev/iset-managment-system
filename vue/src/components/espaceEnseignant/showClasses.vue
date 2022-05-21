be0ca71b2a32e89b515997530f6b468dc990e9d3
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-22 00:00:11 +0100

    [Vue] show classes

--- vue/src/components/espaceEnseignant/showClasses.vue
+++ vue/src/components/espaceEnseignant/showClasses.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
+  <div class="w-100">
     <div
       class="card card-body my-5 py-5 text-center"
       v-if="Classes.length == 0"
     >
       <h3>il y'a aucune Classe</h3>
     </div>
+
     <b-card class="my-2" v-for="Classe in Classes" :key="Classe.id">
-      
-      <md-tabs style="height: auto;!important">
+      <md-tabs class="text-danger" style="height: auto;!important">
         <md-tab
+          class="text-dark"
           id="tab-home"
           style="height: auto;!important"
           md-label="Classe"
@@ -17,23 +18,69 @@
           <div class="bv-example-row text-center">
             <b-row class="mb-2">
               <b-row>
-                <b-col>
-                  Classe  : {{ Classe.classe.nom }}
-                </b-col>
+                <b-col> Classe : {{ Classe.nom }} </b-col>
               </b-row>
               <b-row>
-                <b-col>
-                  Matiere : {{ Classe.nom }} 
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col>
-                  Abbreveation classe : {{ Classe.classe.abreviation }} 
-                </b-col>
+                <b-col> abreviation : {{ Classe.abreviation }} </b-col>
               </b-row>
             </b-row>
-           
           </div>
+        </md-tab>
+        <md-tab
+          class="text-dark"
+          id="tab-matieres"
+          style="height: auto;!important"
+          md-label="Matieres"
+        >
+          <md-tabs id="mdtbs" style="height: auto;!important">
+            <md-tab
+              v-for="matiere in Classe.matieres"
+              :key="matiere.id"
+              :id="'tab-matieres' + matiere.id"
+              style="height: auto;!important"
+              :md-label="matiere.nom"
+            >
+              <!-- input mark foreach student -->
+              <div
+                class="form-group row"
+                v-for="note in matiere.notes"
+                :key="note.id"
+              >
+                <label
+                  :for="'for' + note.id"
+                  class="col-sm-4 col-form-label"
+                  >{{ note.student.name }}</label
+                >
+
+                <div class="col-sm-8 row">
+                  <div role="group" class="row" style="align-items: center">
+                    <div class="col-md-9">
+                      <input
+                        :id="'for' + note.id"
+                        class="form-control"
+                        type="number"
+                        style="
+                          background-color: rgb(236, 239, 241);
+                          border: 0px !important;
+                        "
+                        min="0"
+                        max="20"
+                        v-model="note.note"
+                      />
+                    </div>
+                    <div class="col-md-3">
+                      <div class="input-group-append">
+                        <b-button type="button" variant="success"
+                          @click="setNote(matiere.id, note.student.id, note.note)"
+                          >save</b-button
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </md-tab>
+          </md-tabs>
         </md-tab>
       </md-tabs>
     </b-card>
@@ -77,6 +124,7 @@
 
 <script>
 export default {
+  emits: ["alertIt"],
   props: {
     Classes: Array,
     pagination: Object,
@@ -86,7 +134,7 @@ export default {
       id: "",
     };
   },
-  
+
   emits: ["deleteClasse", "updateClasse", "fetchClasse"],
   methods: {
     Delete(id) {
@@ -100,11 +148,42 @@ export default {
       this.$emit("fetchClasse", url);
     },
     addmat(id) {
-      
-      this.$router.push({ name:"consulterMatiere", params:{
-            id: id,
-          }});
-    }
+      this.$router.push({
+        name: "consulterMatiere",
+        params: {
+          id: id,
+        },
+      });
+    },
+    setNote(idMat,idStudent,Note){
+      this.$http.post(
+        "http://localhost:8000/api/note/",{
+          student_id: idStudent,
+          matiere_id: idMat,
+          note: Note
+        }
+      ).then(response => {
+        this.alertIt({
+          variant: "success",
+          msg: "Note ajouté avec succès",
+          dismissCountDown: 3,
+
+        });
+        console.log(response.data);
+        console.log(idMat,idStudent,Note);
+      });
+    },
+    fetchNote(idMat,idStudent){
+      this.$http.get(
+        "http://localhost:8000/api/note/"+idMat+"/"+idStudent
+      ).then(response => {
+        console.log(response.data.note);
+        return response.data.note;
+      });
+    },
+    alertIt(alert) {
+      this.$emit("alertIt", alert);
+    },
   },
 };
 </script>
