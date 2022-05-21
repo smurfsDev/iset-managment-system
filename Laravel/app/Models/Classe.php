134114ae4acbc71ce9742f66f89dcc6e9b41c8fc
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-21 20:42:59 +0100

    [Laravel] add relations users/department/matieres to classe

--- Laravel/app/Models/Classe.php
+++ Laravel/app/Models/Classe.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use RoleUser;
 
 class Classe extends Model
 {
@@ -14,5 +15,22 @@ class Classe extends Model
         'nom',
         'departement_id',
     ];
+
+    public function departement()
+    {
+        return $this->belongsTo(Departement::class);
+    }
+
+    public function matieres()
+    {
+        return $this->hasMany(Matiere::class, 'idClasse', 'id');
+    }
+
+    public function users()
+    {
+        return $this->hasMany('App\Models\RoleUser','classe','id');
+    }
+
+
 
 }
