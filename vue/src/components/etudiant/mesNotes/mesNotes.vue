5bebc4f4adf4b8015d52a7e09e058b6c3b6676ea
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-22 19:46:35 +0100

    [Vue] fix data recovery

--- vue/src/components/etudiant/mesNotes/mesNotes.vue
+++ vue/src/components/etudiant/mesNotes/mesNotes.vue
@@ -82,7 +82,7 @@ export default {
       let vm = this;
       this.$http.get(page_url)
       .then((res)=> {
-        this.Notes = res.data.data;
+        this.Notes = res.data;
         this.show = false;
         this.edit=false;
         this.alert=alert
