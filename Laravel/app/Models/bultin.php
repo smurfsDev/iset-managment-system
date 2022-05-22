3761e3968f0b441e4abe7267aeee7745cb2fe86a
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-22 21:20:32 +0100

    [Laravel] create bultin model

---
+++ Laravel/app/Models/bultin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bultin extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'moyenne',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+
+
+
+}
