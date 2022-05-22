f2dc563e281bfa375898fa66eee3eed896ce42e9
raed charrad <raed.charrad91@gmail.com>
2022-05-22 22:14:36 +0100

    [Laravel] add receiver_id to messages table

--- Laravel/database/migrations/2022_05_22_124841_create_messages_table.php
+++ Laravel/database/migrations/2022_05_22_124841_create_messages_table.php
@@ -15,11 +15,20 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
+            $table->foreignId('user_id');
+            $table->foreignId('receiver_id');
             $table->text('body');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+            $table->foreign('receiver_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+
         });
     }
 
