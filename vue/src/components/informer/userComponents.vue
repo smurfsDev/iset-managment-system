d6c1c45f820b75b9803644fabcbe60a1c14dc487
raed charrad <raed.charrad91@gmail.com>
2022-05-22 22:19:34 +0100

    [Vue] add fetch users ad messages

--- vue/src/components/informer/userComponents.vue
+++ vue/src/components/informer/userComponents.vue
@@ -3,8 +3,8 @@
         <div class="card">
             <div class="card-header">Users</div>
             <div class="card-body">
-                <div class="users">
-                    <a href="#">Krunal</a>
+                <div class="users" v-for="user in users" :key="user.id">
+                    <a @click="submitMessage(user.id)">{{ user.name }}</a>
                 </div>
             </div>
         </div>
@@ -13,9 +13,37 @@
 
 <script>
     export default {
+        data() {
+            return {
+                users: [],
+                messages: [],
+            }
+        },
+        emits: ["messages"],
         mounted() {
             console.log('Component mounted.')
+        },
+        created() {
+            this.fetchUsers();
+        },
+        methods: {
+            fetchUsers() {
+                this.$http.get("http://localhost:8000/api/message/users").then(response => {
+                    this.users = response.data;
+                })
+            },
+            submitMessage(id) {
+                this.$http.get("http://localhost:8000/api/message/M/"+id).then(response => {
+                this.messages = response.data;
+                this.$emit('messages', response.data);
+                })
+                .catch(() => {
+                    this.messages = [];
+                    this.$emit('messages', []);
+                });
+            }
         }
+
     }
 </script>
 <style>
