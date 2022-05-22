32d3e5d005e325b38c31c82c9add413d0fa3f50f
raed charrad <raed.charrad91@gmail.com>
2022-05-22 14:57:36 +0100

    [Vue] create chatMessagesComponents

---
+++ vue/src/components/informer/chatMessagesComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="message-area">
+        <messageComponent></messageComponent>
+    </div>
+</template>
+
+<script>
+    import messageComponent from './MessageComponent.vue'
+    export default {
+        components: {
+            messageComponent,
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+    
+</script>
+<style>
+    .message-area {
+        height: 400px;
+        max-height: 400px;
+        overflow-y: scroll;
+        padding: 15px;
+        border-bottom: 1px solid #eee;
+    }
+</style>
