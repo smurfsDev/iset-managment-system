950fe0e0657680849673b6eb21b62f17bdc036c7
raed charrad <raed.charrad91@gmail.com>
2022-05-22 14:56:42 +0100

    [Vue] create chatComponents

---
+++ vue/src/components/informer/chatComponents.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Chats</div>
+            <div class="card-body">
+                <chatMessagesComponent></chatMessagesComponent>
+                <chatFormComponent></chatFormComponent>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import chatMessagesComponent from './chatMessagesComponent';
+import chatFormComponent from "./chatFormComponent";
+
+    export default {
+        components: {
+        chatMessagesComponent,
+        chatFormComponent,
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
