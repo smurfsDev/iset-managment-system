53f02f1400f10878f714eedaef7389909a31e959
raed charrad <raed.charrad91@gmail.com>
2022-05-22 22:19:03 +0100

    [Vue]  print msgs dynamicly

--- vue/src/components/informer/MessageComponent.vue
+++ vue/src/components/informer/MessageComponent.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="message self">
-        <strong class="user">Krunal</strong>
-        <p class="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cumque quaerat rem quia veniam exercitationem, commodi numquam omnis! Non placeat perspiciatis nulla illum cumque ad natus asperiores fuga. Facere, dignissimos.</p>
+        <strong class="user">{{ message.user.name }}</strong>
+        <p class="body">{{ message.body }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+    props: {
+        message: Object
+    },
+    
+  }
 </script>
 <style>
     .user {
