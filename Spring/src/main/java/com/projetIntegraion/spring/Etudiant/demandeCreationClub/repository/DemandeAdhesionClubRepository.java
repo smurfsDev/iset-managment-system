6c56c3121f193d5f496ead82b42d530c9a8e8a48
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-25 18:49:06 +0100

    [Spring]  find by club and etudiant id

--- Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeAdhesionClubRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeAdhesionClubRepository.java
@@ -2,9 +2,13 @@ package com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository;
 
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.DemandeAdhesionClub;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public interface DemandeAdhesionClubRepository extends JpaRepository<DemandeAdhesionClub, Long>{
-    DemandeAdhesionClub findByClubIdAndEtudiantId(Long id, Long id2);
+    DemandeAdhesionClub findByClubIdAndEtudiantId(Long id, int id2);
+    Page<DemandeAdhesionClub> findByEtudiantId(int id, Pageable pageable);
+    Page<DemandeAdhesionClub> findByClubId(Long id, Pageable pageable);
 }
