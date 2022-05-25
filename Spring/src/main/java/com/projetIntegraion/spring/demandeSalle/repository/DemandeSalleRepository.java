dc045af72396c241f09effa930f966f56e002eeb
raed charrad <raed.charrad91@gmail.com>
2022-05-25 23:09:24 +0100

    [Spring] add findByIdResponsable

--- Spring/src/main/java/com/projetIntegraion/spring/demandeSalle/repository/DemandeSalleRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandeSalle/repository/DemandeSalleRepository.java
@@ -2,11 +2,14 @@ package com.projetIntegraion.spring.demandeSalle.repository;
 
 import com.projetIntegraion.spring.demandeSalle.entity.DemandeSalle;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories  
+@EnableJpaRepositories
 
-public interface DemandeSalleRepository extends JpaRepository<DemandeSalle, Long>{
+public interface DemandeSalleRepository extends JpaRepository<DemandeSalle, Long> {
+    Page<DemandeSalle> findByIdResponsable(Integer id, Pageable pageable);
 
 }
