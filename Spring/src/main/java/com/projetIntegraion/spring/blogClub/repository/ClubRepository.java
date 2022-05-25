e010f049be33c4d86be2e261e8c7f5b8eb5d70d7
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-25 22:43:59 +0100

    [Spring] add findByResponsableClub method

--- Spring/src/main/java/com/projetIntegraion/spring/blogClub/repository/ClubRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/blogClub/repository/ClubRepository.java
@@ -2,6 +2,7 @@ package com.projetIntegraion.spring.blogClub.repository;
 
 import java.util.Optional;
 
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 import com.projetIntegraion.spring.blogClub.entity.Club;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ public interface ClubRepository extends JpaRepository<Club, Long>{
     // Page<Club> findByresponsableClubId(Long id,Pageable pageable);
     // Page<Club> findByNomClubContains(String nomClub,Pageable pageable);
     Optional<Club> findByResponsableClubId(int id);
+    Optional<Club> findByResponsableClub(User id);
+
 }
