f1a02dd048d49241cbd0c6306759fa3bd259169e
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-25 15:56:50 +0100

    [Spring] add user seeding

--- Spring/src/main/java/com/projetIntegraion/spring/Application.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Application.java
@@ -2,19 +2,15 @@ package com.projetIntegraion.spring;
 
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.Role;
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
-import com.projetIntegraion.spring.Etudiant.demandeCreationClub.service.RoleService;
 import com.projetIntegraion.spring.blogClub.service.ActivitiesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository.RoleRepository;
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository.UserRepository;
-import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -33,20 +29,20 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		if (roleRepository.findAll().size() != 0) {
-			List<Role> roles = roleRepository.findAll();
-			for (Role role : roles) {
-				System.out.println(role.getId());
-				roleRepository.deleteById(role.getId());
-			}
-		}
+		// if (userRepository.findAll().size() != 0) {
+		// 	List<User> users = userRepository.findAll();
+		// 	for (User user : users) {
+		// 		userRepository.deleteById(user.getId());
+		// 	}
+		// }
+		// if (roleRepository.findAll().size() != 0) {
+		// 	List<Role> roles = roleRepository.findAll();
+		// 	for (Role role : roles) {
+		// 		System.out.println(role.getId());
+		// 		roleRepository.deleteById(role.getId());
+		// 	}
+		// }
 
-		if (userRepository.findAll().size() != 0) {
-			List<User> users = userRepository.findAll();
-			for (User user : users) {
-				userRepository.deleteById(user.getId());
-			}
-		}
 
 		Role role = new Role("ROLE_ADMIN");
 		Role role1 = new Role("ROLE_RESPONSABLE");
@@ -54,6 +50,25 @@ public class Application implements CommandLineRunner {
 		roleRepository.save(role1);
 		roleRepository.save(role2);
 		roleRepository.save(role);
+
+		User admin = new User();
+		admin.setUsername("admin@example.com");
+		admin.setPassword(new BCryptPasswordEncoder().encode("password"));
+		admin.getRoles().add(role);
+
+		User responsable = new User();
+		responsable.setUsername("responsable@example.com");
+		responsable.setPassword(new BCryptPasswordEncoder().encode("password"));
+		responsable.getRoles().add(role1);
+
+		User student = new User();
+		student.setUsername("student@example.com");
+		student.setPassword(new BCryptPasswordEncoder().encode("password"));
+		student.getRoles().add(role2);
+
+		userRepository.save(admin);
+		userRepository.save(responsable);
+		userRepository.save(student);
 
 	}
 
