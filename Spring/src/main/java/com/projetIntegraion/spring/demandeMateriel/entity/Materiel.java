77ad6cdd5cb56a46406d3ce4fd9974a05663c1d7
raed charrad <raed.charrad91@gmail.com>
2022-05-25 23:04:42 +0100

    [Spring] update Materiel Entity

--- Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/entity/Materiel.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/entity/Materiel.java
@@ -15,8 +15,9 @@ import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 
 @Entity
 public class Materiel {
-	@Id @GeneratedValue 
-	private  Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 	@NotNull
 	@Size(min = 3, max = 30)
 	private String titre;
@@ -30,62 +31,81 @@ public class Materiel {
 	@ManyToOne
 	private CategorieMateriel categorie;
 
-	@OneToMany(mappedBy="materiel", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "materiel", cascade = CascadeType.ALL)
 	private List<DemandeMaterielMateriel> demandeMateriel;
-	
+
 	public Materiel(@NotNull @Size(min = 3, max = 30) String titre, @NotNull int quantite,
-			@NotNull @Size(min = 3, max = 150) String description) {
-		super();
-		
+			@NotNull @Size(min = 3, max = 150) String description, User responsable, CategorieMateriel categorie,
+			List<DemandeMaterielMateriel> demandeMateriel) {
+		this.titre = titre;
+		this.quantite = quantite;
+		this.description = description;
+		this.responsable = responsable;
+		this.categorie = categorie;
+		this.demandeMateriel = demandeMateriel;
 	}
+
 	public String getTitre() {
 		return titre;
 	}
+
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
+
 	public int getQuantite() {
 		return quantite;
 	}
+
 	public void setQuantite(int quantite) {
 		this.quantite = quantite;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public User getResponsable() {
 		return responsable;
 	}
+
 	public void setResponsable(User responsable) {
 		this.responsable = responsable;
 	}
+
 	public CategorieMateriel getCategorie() {
 		return categorie;
 	}
+
 	public void setCategorie(CategorieMateriel categorie) {
 		this.categorie = categorie;
 	}
+
 	public List<DemandeMaterielMateriel> getDemandeMateriel() {
 		return demandeMateriel;
 	}
+
 	public void setDemandeMateriel(List<DemandeMaterielMateriel> demandeMateriel) {
 		this.demandeMateriel = demandeMateriel;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public Materiel() {
 		super();
 	}
+
 	@Override
 	public String toString() {
 		return "Materiel [id=" + id + ", titre=" + titre + ", quantite=" + quantite + ", description=" + description
 				+ ", responsable=" + responsable + ", categorie=" + categorie + ", demandeMateriel=" + demandeMateriel
 				+ "]";
 	}
-	
-	
+
 }
