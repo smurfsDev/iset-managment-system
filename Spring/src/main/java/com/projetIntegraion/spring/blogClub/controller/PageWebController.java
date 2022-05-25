125fcad7a3ac9886551950068746cdbd6cadc698
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-05-26 01:39:27 +0200

    [Spring] show page club

--- Spring/src/main/java/com/projetIntegraion/spring/blogClub/controller/PageWebController.java
+++ Spring/src/main/java/com/projetIntegraion/spring/blogClub/controller/PageWebController.java
@@ -1,7 +1,12 @@
 package com.projetIntegraion.spring.blogClub.controller;
 
+import java.security.Principal;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository.UserRepository;
 import com.projetIntegraion.spring.blogClub.entity.About;
 import com.projetIntegraion.spring.blogClub.entity.Activity;
 import com.projetIntegraion.spring.blogClub.entity.Board;
@@ -32,13 +37,26 @@ public class PageWebController {
     ProjectService projectService;
     @Autowired
     ClubService clubService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUser(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        User user = userRepository.findUserWithName(principal.getName()).get();
+        return user;
+    }
     @RequestMapping("/blogClub")
     public String showPage(ModelMap modelMap,
+        HttpServletRequest request,
         @RequestParam(name = "idClub", defaultValue = "1") Long idClub
     ) {
                
        // Page<About> listDcc = aboutService.getAllAboutsParPage(page, size);
-       Club c = clubService.getClub(idClub);
+       
+       //Club c = clubService.getClub(idClub);
+       Club c = clubService.getClubParResponsable(this.getUser(request).getId()).get();
+       
        List<About> Abouts = aboutService.getAboutByClubId(c);
        List<Activity> Activities = activitiesService.getActivityByClubId(c);
        List<Board> Boards = boardService.getBoardByClubId(c);
