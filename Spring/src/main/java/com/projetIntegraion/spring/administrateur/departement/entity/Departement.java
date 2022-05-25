091b037300e7177344aab5e90095a9e3d989e0e0
raed charrad <raed.charrad91@gmail.com>
2022-05-25 23:02:01 +0100

    [Spring] update Departement Entity

--- Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/entity/Departement.java
+++ Spring/src/main/java/com/projetIntegraion/spring/administrateur/departement/entity/Departement.java
@@ -13,61 +13,68 @@ import javax.validation.constraints.Size;
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 import com.projetIntegraion.spring.demandeSalle.entity.Salle;
 
-@Entity 
+@Entity
 public class Departement {
-    
-    @Id @GeneratedValue 
-	private  Long id;
+
+	@Id
+	@GeneratedValue
+	private Long id;
 	@NotNull
 	@Size(min = 3, max = 30)
 	private String titre;
 	@OneToOne
 	private User ChefDepartment;
 	@OneToMany(mappedBy = "departement")
-    private List<Salle> Salle;
-	
+	private List<Salle> Salle;
+
 	public Departement() {
 		super();
 	}
-	public Departement(Long id, String titre, User chefDepartment, List<Salle> salle) {
+
+	public Departement(String titre, User chefDepartment, List<Salle> salle) {
 		super();
-		this.id = id;
+
 		this.titre = titre;
 		ChefDepartment = chefDepartment;
 		Salle = salle;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitre() {
 		return titre;
 	}
+
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
+
 	public User getChefDepartment() {
 		return ChefDepartment;
 	}
+
 	public void setChefDepartment(User chefDepartment) {
 		ChefDepartment = chefDepartment;
 	}
+
 	public List<Salle> getSalle() {
 		return Salle;
 	}
+
 	public void setSalle(List<Salle> salle) {
 		Salle = salle;
 	}
+
 	@Override
 	public String toString() {
 		return "Departement [id=" + id + ", titre=" + titre + ", ChefDepartment=" + ChefDepartment + ", Salle=" + Salle
 				+ "]";
 	}
 
-
-
-
-    
 }
