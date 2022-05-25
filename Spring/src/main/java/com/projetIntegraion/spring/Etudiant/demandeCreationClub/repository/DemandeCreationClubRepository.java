0f886c6072f034c80577ed05f98ab536a3459eee
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-25 18:49:18 +0100

    [Spring] find by resClub

--- Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeCreationClubRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/DemandeCreationClubRepository.java
@@ -1,6 +1,7 @@
 package com.projetIntegraion.spring.Etudiant.demandeCreationClub.repository;
 
 import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.DemandeCreationClub;
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public interface DemandeCreationClubRepository extends JpaRepository<DemandeCreationClub, Long>{
-    Page<DemandeCreationClub> findByresponsableClubId(Long id,Pageable pageable);
+    // Page<DemandeCreationClub> findByresponsableClubId(Long id,Pageable pageable);
     Page<DemandeCreationClub> findByNomClubContains(String nomClub,Pageable pageable);
+    Page<DemandeCreationClub> findByResponsableClub(User responsableClub,Pageable pageable);
 }
