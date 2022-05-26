44f82c8599291868af57552a28ff2b1f92cbeadc
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-26 17:19:02 +0100

    [Laravel]Modifier Document Model

--- Laravel/app/Models/Document.php
+++ Laravel/app/Models/Document.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
     use HasFactory;
+
     protected $table = 'documents';
     protected $fillable = [
         'nom', 
-        'class', 
-        'categorie', 
+        'class_id', 
+        'document_categories_id', 
         'file', 
         'idResponsable'
     ];
