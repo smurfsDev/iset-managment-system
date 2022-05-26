678e14e01211b9ac558663834ffe279597d1b881
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-26 17:16:12 +0100

    [Laravel]Creation Reclamationcategorie Controller

---
+++ Laravel/app/Http/Controllers/TypeCatController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeCat;
+use Illuminate\Http\Request;
+
+class TypeCatController extends Controller
+{
+     public function get (){
+        $categories = TypeCat::all();
+       if($categories->isEmpty()){
+           return response()->json(['message' => 'Aucune catégorie de document n\'est disponible'], 404);
+        }
+        return response()->json($categories, 200);
+}
+      public function showDocumentCategorie(){
+        $DC = TypeCat::orderBy("updated_at","desc")->paginate(5);
+        if(sizeof($DC)>0){
+        return response()->json(["data"=>$DC], 200);
+        }
+        else
+            return response()->json([
+                "aucune Categorie"
+            ], 404);
+    }
+
+     function createDocumentCategorie(Request $request){
+        $idResponsable = $request->user()->id;
+        $title = $request->input('title');
+        
+        $newDC= array(
+            "title" => $title,
+            "idResponsable" => $idResponsable
+        );
+
+         $createNewDC = TypeCat::create($newDC);
+
+        if ($createNewDC) {
+            return response()->json([
+                'data' => [
+                    'message' => 'Categorie Ajouté avec succès',
+                    'id' => $createNewDC->id,
+                    'attributes' => $createNewDC
+                ]
+            ], 201);
+        }
+        return "Categorie Ajouté avec succès";
+}
+
+ public function updateDocumentCategorie($id, Request $request){
+
+        $dc = TypeCat::find($id);
+        if ($dc) {
+
+            $dc->title = $request->input('title') ? $request->input('title') : $dc->title;
+           
+            $dc->save();
+            return response()->json([
+                'message' => 'Categorie mis à jour',
+                'id' => $dc->id,
+                'attributes' => $dc
+            ], 201);
+        } else {
+            return response()->json([
+                "Categorie n'existe pas"
+            ], 404);
+        }
+    }
+    
+public function deleteDocumentCategorie($id){
+
+        $dc = TypeCat::find($id);
+
+        if ($dc){
+            $dc->delete();
+            return response()->json([
+                'type' => 'CategorieDocument',
+                'message' => 'Categorie Supprimé! '
+            ], 204);
+        }
+        else {
+            return response()->json([
+                'type' => 'CategorieDocument',
+                'message' => "Categorie n'existe pas"
+            ], 404);
+        }
+    }
+}
