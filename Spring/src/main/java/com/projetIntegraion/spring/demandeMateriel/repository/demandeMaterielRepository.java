5d64f544da5d1bb016759c4166039f0316e386cc
raed charrad <raed.charrad91@gmail.com>
2022-05-26 17:42:21 +0100

    [Spring] add findByResponsableClub function

--- Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/repository/demandeMaterielRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/repository/demandeMaterielRepository.java
@@ -1,6 +1,6 @@
 package com.projetIntegraion.spring.demandeMateriel.repository;
 
-
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 import com.projetIntegraion.spring.demandeMateriel.entity.DemandeMateriel;
 
 import org.springframework.data.domain.Page;
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-public interface demandeMaterielRepository extends JpaRepository<DemandeMateriel, Long>{
-    Page<DemandeMateriel> findByResponsableClubId(Long id,Pageable pageable);
+public interface demandeMaterielRepository extends JpaRepository<DemandeMateriel, Long> {
+    Page<DemandeMateriel> findByResponsableClub(User id, Pageable pageable);
 }
