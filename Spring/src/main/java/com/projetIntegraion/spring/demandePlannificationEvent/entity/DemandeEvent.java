ab4170d8988eedecd2532d0954c083f440934b38
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-05-26 22:26:55 +0200

    [Spring] form createDemandeEvent

--- Spring/src/main/java/com/projetIntegraion/spring/demandePlannificationEvent/entity/DemandeEvent.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandePlannificationEvent/entity/DemandeEvent.java
@@ -1,11 +1,14 @@
 package com.projetIntegraion.spring.demandePlannificationEvent.entity;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -32,6 +35,8 @@ public class DemandeEvent {
     @NotNull
     private String description;
     private int status;
+    @FutureOrPresent
+    private Date DateEvent;
     @CreationTimestamp
     private Timestamp created_at;
     @UpdateTimestamp
