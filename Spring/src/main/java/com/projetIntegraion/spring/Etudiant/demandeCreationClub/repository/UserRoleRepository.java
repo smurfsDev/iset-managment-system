cb79ec4c00556627585791cbfdbd49e7378e6495
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-26 17:36:54 +0100

    [Spring] add findFirstByUserId to userrole repo

--- Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/UserRoleRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/Etudiant/demandeCreationClub/repository/UserRoleRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import groovy.cli.Option;
+
 @EnableJpaRepositories
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
 
     Optional<UserRole> findByRoleIdAndUserId(Long id, int userId);
+    Optional<UserRole> findFirstByUserId(int userId);
         
 }
