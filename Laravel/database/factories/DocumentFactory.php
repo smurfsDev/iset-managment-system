2f7d380fccaa7456964df8ec5356cad400bfe8b7
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-26 17:22:22 +0100

    [Laravel]Modifier DocumentCategorie Factory

--- Laravel/database/factories/DocumentFactory.php
+++ Laravel/database/factories/DocumentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Classe;
+use App\Models\DocumentCategorie;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -15,12 +17,12 @@ class DocumentFactory extends Factory
     public function definition()
     {
         return [
-            'nom' => $this->faker->sentence,
-            'class' => $this->faker->sentence,
-            'categorie' => $this->faker->sentence,
+            'nom' => $this->faker->word,
+            'classes_id' => Classe::get('id')->random(),
+            'document_categories_id' => DocumentCategorie::get('id')->random(), 
             'file' => $this->faker->sentence,
             'idResponsable' =>User::get('id')->random(),
-
+            
         ];
     }
 }
