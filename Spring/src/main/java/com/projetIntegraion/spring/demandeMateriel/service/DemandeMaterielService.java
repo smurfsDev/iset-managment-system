c7725a182356ca61e2112076507f04f4b21e5076
raed charrad <raed.charrad91@gmail.com>
2022-05-26 17:43:00 +0100

    [Spring] add getAllDemandeSalleParResponsableClub

--- Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/service/DemandeMaterielService.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandeMateriel/service/DemandeMaterielService.java
@@ -1,7 +1,8 @@
-	package com.projetIntegraion.spring.demandeMateriel.service;
+package com.projetIntegraion.spring.demandeMateriel.service;
 
 import java.util.List;
 
+import com.projetIntegraion.spring.Etudiant.demandeCreationClub.entity.User;
 import com.projetIntegraion.spring.demandeMateriel.entity.DemandeMateriel;
 import com.projetIntegraion.spring.demandeMateriel.repository.demandeMaterielRepository;
 
@@ -13,33 +14,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemandeMaterielService {
 
-	@Autowired  
+	@Autowired
 	demandeMaterielRepository demandeMaterielRepository;
-	
-	public DemandeMateriel save(DemandeMateriel demande){
+
+	public DemandeMateriel save(DemandeMateriel demande) {
 		return demandeMaterielRepository.save(demande);
 	}
-	public boolean existsById(Long id){
+
+	public boolean existsById(Long id) {
 		return demandeMaterielRepository.existsById(id);
 	}
-	public void delete(DemandeMateriel demande){
+
+	public void delete(DemandeMateriel demande) {
 		demandeMaterielRepository.delete(demande);
 	}
-	public boolean deleteById(Long id){
+
+	public boolean deleteById(Long id) {
 		if (demandeMaterielRepository.existsById(id)) {
 			demandeMaterielRepository.deleteById(id);
 			return true;
 		}
 		return false;
 	}
-	public DemandeMateriel getdemandeById(Long id){
+
+	public DemandeMateriel getdemandeById(Long id) {
 		return demandeMaterielRepository.existsById(id) ? demandeMaterielRepository.findById(id).get() : null;
 	}
-	public List<DemandeMateriel> getAlldemande(){
+
+	public List<DemandeMateriel> getAlldemande() {
 		return demandeMaterielRepository.findAll();
 	}
-	public Page<DemandeMateriel> getAllDemandeParPage(int page , int size){
+
+	public Page<DemandeMateriel> getAllDemandeParPage(int page, int size) {
 		return demandeMaterielRepository.findAll(PageRequest.of(page, size));
 	}
-	
+
+	public Page<DemandeMateriel> getAllDemandeSalleParPageParResponsableClub(User use, int page, int size) {
+		return demandeMaterielRepository.findByResponsableClub(use, PageRequest.of(page, size));
+	}
+
 }
