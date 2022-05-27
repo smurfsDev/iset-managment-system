68e46b476cef3ec9f3776849efd7cb5464e7cced
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-05-27 02:45:20 +0200

    [Spring] create DemandeAdhesionEventService

---
+++ Spring/src/main/java/com/projetIntegraion/spring/demandePlannificationEvent/service/DemandeAdhesionEventService.java
@@ -0,0 +1,63 @@
+package com.projetIntegraion.spring.demandePlannificationEvent.service;
+
+import java.util.List;
+
+import com.projetIntegraion.spring.demandePlannificationEvent.entity.DemandeAdhesionEvent;
+import com.projetIntegraion.spring.demandePlannificationEvent.repository.DemandeAdhesionEventRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public class DemandeAdhesionEventService {
+    @Autowired
+	DemandeAdhesionEventRepository DemandeAdhesionEventRepository;
+
+	public DemandeAdhesionEvent save(DemandeAdhesionEvent p) {
+		return DemandeAdhesionEventRepository.save(p);
+	}
+
+	public Boolean existsById(Long id) {
+		return DemandeAdhesionEventRepository.existsById(id);
+	}
+
+	public Boolean existsByIds(Long idClub, int idEtudiant) {
+		return DemandeAdhesionEventRepository.findByClubIdAndEtudiantId(idClub, idEtudiant)!=null;
+	}
+
+	public DemandeAdhesionEvent updateDemandeAdhesionEvent(Long id, DemandeAdhesionEvent p) {
+		return null;
+	}
+
+	public void delete(DemandeAdhesionEvent p) {
+		DemandeAdhesionEventRepository.delete(p);
+	}
+
+	public Boolean deleteById(Long id) {
+		if (DemandeAdhesionEventRepository.existsById(id)) {
+			DemandeAdhesionEventRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	public DemandeAdhesionEvent getDemandeAdhesionEvent(Long id) {
+		return DemandeAdhesionEventRepository.existsById(id) == false ? null : DemandeAdhesionEventRepository.findById(id).get();
+	}
+
+	public List<DemandeAdhesionEvent> getAllDemandeAdhesionEvent() {
+		return DemandeAdhesionEventRepository.findAll();
+	}
+
+	public Page<DemandeAdhesionEvent> getAllDemandeAdhesionEventParPage(int page, int size){
+		return DemandeAdhesionEventRepository.findAll(PageRequest.of(page, size));
+	};
+
+	public Page<DemandeAdhesionEvent> getAllDemandeAdhesionEventParPageEtudiant(int id, int page, int size){
+		return DemandeAdhesionEventRepository.findByEtudiantId(id, PageRequest.of(page, size));
+	};
+
+	public Page<DemandeAdhesionEvent> getAllDemandeAdhesionEventParPageClub(Long id, int page, int size){
+		return DemandeAdhesionEventRepository.findByClubId(id, PageRequest.of(page, size));
+	};
+}
