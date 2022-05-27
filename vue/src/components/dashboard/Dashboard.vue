a4988ae60e983c6adcb9de9f4fae3570a709f750
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-27 17:48:47 +0100

    [Vue] create dashboard main component

---
+++ vue/src/components/dashboard/Dashboard.vue
@@ -0,0 +1,286 @@
+<template>
+  <div>
+    <div class="main-content">
+      <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+        <div class="container-fluid">
+          <h2 class="mb-5 text-white">Stats Card</h2>
+          <div class="header-body">
+            <div class="row">
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          Traffic
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">350,897</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-danger
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fas fa-chart-bar"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-success mr-2"
+                        ><i class="fa fa-arrow-up"></i> 3.48%</span
+                      >
+                      <span class="text-nowrap">Since last month</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          New users
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">2,354</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-warning
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fas fa-chart-pie"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-danger mr-2"
+                        ><i class="fas fa-arrow-down"></i> 3.48%</span
+                      >
+                      <span class="text-nowrap">Since last week</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          Sales
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">924</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-dark
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fas fa-users"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-warning mr-2"
+                        ><i class="fas fa-arrow-down"></i> 1.10%</span
+                      >
+                      <span class="text-nowrap">Since yesterday</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          Performance
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">49,65%</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-info
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fas fa-percent"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-success mr-2"
+                        ><i class="fas fa-arrow-up"></i> 12%</span
+                      >
+                      <span class="text-nowrap">Since last month</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          Sales
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">924</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-dark
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fa fa-users"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-warning mr-2"
+                        ><i class="fas fa-arrow-down"></i> 1.10%</span
+                      >
+                      <span class="text-nowrap">Since yesterday</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mycrd col-xl-3 col-lg-4">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">
+                          Performance
+                        </h5>
+                        <span class="h2 font-weight-bold mb-0">49,65%</span>
+                      </div>
+                      <div class="col-auto">
+                        <div
+                          class="
+                            icon icon-shape
+                            bg-info
+                            text-white
+                            rounded-circle
+                            shadow
+                          "
+                        >
+                          <i class="fas fa-percent"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                      <span class="text-success mr-2"
+                        ><i class="fas fa-arrow-up"></i> 12%</span
+                      >
+                      <span class="text-nowrap">Since last month</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row my-5">
+              <div class="row my-2 mx-4">
+                <div class="col-5  bg-light rounded">
+                  <line-chart label="a" :labels="['Item 1', 'Item 2', 'Item 3']" :data="[10, 20, 30]"></line-chart>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-5 bg-light rounded">
+                  <bar-chart label="a" :labels="['Item 1', 'Item 2', 'Item 3']" :data="[10, 20, 30]" ></bar-chart>
+                </div>
+              </div>
+              <div class="row my-2 mx-4">
+                <div class="col-5  bg-light rounded">
+                  <area-chart label="a" :labels="['Item 1', 'Item 2', 'Item 3']" :data="[10, 20, 30]" ></area-chart>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-5 bg-light rounded">
+                  <bar-chart label="a" :labels="['Item 1', 'Item 2', 'Item 3']" :data="[10, 20, 30]" ></bar-chart>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Page content -->
+    </div>
+  </div>
+</template>
+
+<script>
+import barChart from "./barChart.vue";
+import lineChart from "./lineChart.vue";
+import areaChart from "./areaChart.vue";
+export default {
+  components: {
+    barChart,
+    lineChart,
+    areaChart,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.icon {
+  width: 3rem;
+  height: 3rem;
+}
+
+.icon i {
+  font-size: 2.25rem;
+}
+
+.icon-shape {
+  display: inline-flex;
+  padding: 12px;
+  text-align: center;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon-shape i {
+  font-size: 1.25rem;
+}
+.bg-gradient-primary {
+  background: linear-gradient(87deg, #5e72e4 0, #825ee4 100%) !important;
+}
+
+.bg-gradient-primary {
+  background: linear-gradient(87deg, #5e72e4 0, #825ee4 100%) !important;
+}
+
+.mycrd {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
