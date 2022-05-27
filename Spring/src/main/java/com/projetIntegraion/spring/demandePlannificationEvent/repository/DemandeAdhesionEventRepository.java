5e4bd886a62bc50d5b69f14313d1ba5cfb938b89
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-05-27 02:40:46 +0200

    [Spring] add method in DemandeAdhesionEvent repo

--- Spring/src/main/java/com/projetIntegraion/spring/demandePlannificationEvent/repository/DemandeAdhesionEventRepository.java
+++ Spring/src/main/java/com/projetIntegraion/spring/demandePlannificationEvent/repository/DemandeAdhesionEventRepository.java
@@ -2,10 +2,15 @@ package com.projetIntegraion.spring.demandePlannificationEvent.repository;
 
 import com.projetIntegraion.spring.demandePlannificationEvent.entity.DemandeAdhesionEvent;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 public interface DemandeAdhesionEventRepository extends JpaRepository<DemandeAdhesionEvent, Long>  {
-    
+    DemandeAdhesionEvent findByClubIdAndEtudiantId(Long id, int id2);
+    Page<DemandeAdhesionEvent> findByEtudiantId(int id, Pageable pageable);
+    Page<DemandeAdhesionEvent> findByClubId(Long id, Pageable pageable);
 }
