2c307c37208b6e74aabda885b8d0edba22c86709
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-27 17:48:16 +0100

    [vue] create barchart component

---
+++ vue/src/components/dashboard/barChart.vue
@@ -0,0 +1,37 @@
+<script>
+import { Bar } from 'vue-chartjs';
+export default {
+    extends: Bar,
+    props: ['labels', 'data','label'],
+    mounted() {
+        this.renderChart(
+            {
+                labels: this.labels,
+                datasets: [
+                    {
+                        label: this.label,
+                        data: this.data,
+                        backgroundColor: [
+                            '#ffcdd2',
+                            '#f8bbd0',
+                            '#d1c4e9',
+                            '#c5cae9',
+                        ],
+                        borderColor: "white",
+                        pointBackgroundColor: "white",
+                    }
+                ]
+            },
+            {
+                responsive: true,
+                maintainAspectRatio: false,
+                title: {
+                    display: true,
+                    text: "My Data"
+                },
+                fontColor: 'red'
+            },
+        );
+    }
+};
+</script>
