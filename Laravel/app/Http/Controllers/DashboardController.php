60ef5d429437a4b29a4a26038b6aea04ca261cde
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-27 17:47:33 +0100

    [Laravel] create dashboard controller

---
+++ Laravel/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function get(Request $request)
+    {
+
+
+
+    }
+}
