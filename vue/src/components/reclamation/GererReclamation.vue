d9bf9ab3f7fdc794f72ea395c1efc5e0a514f533
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-27 14:19:13 +0100

    [Vue] FIX gerer Reclamation componnents

--- vue/src/components/reclamation/GererReclamation.vue
+++ vue/src/components/reclamation/GererReclamation.vue
@@ -1,31 +1,54 @@
 <template>
   <div>
-    <formReclamation @addReclamation="addReclamation" :oldReclamation="Reclamation" />
+    <formReclamation
+      @addReclamation="addReclamation"
+      :oldReclamation="Reclamation"
+    />
     <div class="content container">
       <div class="pt-3 pb-3 container-fluid">
-        <b-overlay v-if="show" :show="show" class="d-inline-block" style="height: 500px; width: 100%"></b-overlay>
+        <b-overlay
+          v-if="show"
+          :show="show"
+          class="d-inline-block"
+          style="height: 500px; width: 100%"
+        ></b-overlay>
         <div v-if="!show">
           <b-container class="bv-example-row py-0">
             <b-row class="text-center">
               <b-col cols="8">
-                <button 
-                v-if="!($store.getters.isAdmin)"
-                type="button" class="btn btn-primary mx-1 float-start" data-bs-toggle="modal"
-                  @click="initModal()" data-bs-target="#ReclamationModal">
+                <button
+                  v-if="!$store.getters.isAdmin"
+                  type="button"
+                  class="btn btn-primary mx-1 float-start"
+                  data-bs-toggle="modal"
+                  @click="initModal()"
+                  data-bs-target="#ReclamationModal"
+                >
                   Nouvelle Reclamation
                 </button>
               </b-col>
               <b-col></b-col>
             </b-row>
           </b-container>
-          <b-alert class="mt-4" :show="alert.dismissCountDown" dismissible :variant="alert.variant"
-            @dismissed="alert.dismissCountDown = 0">
+          <b-alert
+            class="mt-4"
+            :show="alert.dismissCountDown"
+            dismissible
+            :variant="alert.variant"
+            @dismissed="alert.dismissCountDown = 0"
+          >
             <p>{{ alert.msg }}</p>
           </b-alert>
 
           <!-- <b-card> -->
-          <showReclamations @setReponse="setReponse" @deleteReclamation="deleteReclamation" :Reclamations="Reclamations"
-            @fetchReclamation="fetchReclamation" @updateReclamation="updateReclamation" :pagination="pagination" />
+          <showReclamations
+            @setReponse="setReponse"
+            @deleteReclamation="deleteReclamation"
+            :Reclamations="Reclamations"
+            @fetchReclamation="fetchReclamation"
+            @updateReclamation="updateReclamation"
+            :pagination="pagination"
+          />
           <!-- </b-card> -->
         </div>
       </div>
@@ -81,19 +104,18 @@ export default {
   },
 
   methods: {
-    fetchReclamation(
-      page_url = "http://127.0.0.1:8000/api/reclamation/"
-    ) {
+    fetchReclamation(page_url = "http://127.0.0.1:8000/api/reclamation/") {
       let vm = this;
-      this.$http.get(page_url)
-      .then((res)=> {
-        this.Reclamations = res.data.data;
-        this.show = false;
-        vm.makePagination(res.data);
-      })
-      .finally(()=> {
-        this.show = false;
-      });
+      this.$http
+        .get(page_url)
+        .then((res) => {
+          this.Reclamations = res.data.data;
+          this.show = false;
+          vm.makePagination(res.data);
+        })
+        .finally(() => {
+          this.show = false;
+        });
     },
 
     makePagination(meta) {
@@ -110,13 +132,14 @@ export default {
     deleteReclamation(id) {
       if (confirm("Delete Reclamation " + id)) {
         this.show = true;
-        this.$http.delete("http://localhost:8000/api/reclamation/" + id)
-        .then(() => {
-          this.fetchReclamation();
+        this.$http
+          .delete("http://localhost:8000/api/reclamation/" + id)
+          .then(() => {
+            this.fetchReclamation();
             this.alert.variant = "danger";
             this.alert.msg = "Reclamation suprimée avec succès";
             this.alert.dismissCountDown = 5;
-        });
+          });
       }
     },
 
@@ -125,14 +148,13 @@ export default {
     },
 
     addReclamation(Reclamation) {
-      
       this.show = true;
       if (!this.edit) {
-       this.$http.post('http://localhost:8000/api/reclamation',
-        (Reclamation))
-        .then((data) => {
-          data = data.data;
-          if (data.success == false) {
+        this.$http
+          .post("http://localhost:8000/api/reclamation", Reclamation)
+          .then((data) => {
+            data = data.data;
+            if (data.success == false) {
               this.alert.variant = "danger";
               let err = "";
               for (const property in data.data) {
@@ -148,36 +170,42 @@ export default {
               this.alert.dismissCountDown = 5;
             }
             this.fetchReclamation();
-        });
+          });
       } else {
-        this.$http.put("http://localhost:8000/api/reclamation/" + Reclamation.id,
-        (Reclamation))
-        .then(() => {
-          this.fetchReclamation();
+        this.$http
+          .put(
+            "http://localhost:8000/api/reclamation/" + Reclamation.id,
+            Reclamation
+          )
+          .then(() => {
+            this.fetchReclamation();
             this.edit = false;
             this.alert.variant = "warning";
             this.alert.msg = "Reclamation modifié avec succès";
             this.alert.dismissCountDown = 5;
-        });
+          });
       }
     },
     updateReclamation(Reclamation) {
       this.edit = true;
       this.Reclamation = Reclamation;
     },
-    
+
     initModal() {
       this.Reclamation = {};
       this.showModal("ReclamationModal");
     },
-    setReponse(id,reponse) {
-      this.$http.put("http://127.0.0.1:8000/api/reclamation/reponse/" + id,{reponse:reponse})
+    setReponse(id, reponse) {
+      this.$http
+        .put("http://127.0.0.1:8000/api/reclamation/reponse/" + id, {
+          reponse: reponse,
+        })
         .then((res) => res.data)
         .then(() => {
           this.fetchReclamation();
-            this.alert.variant = "warning";
-            this.alert.msg = "Reponse envoyée avec succès";
-            this.alert.dismissCountDown = 5;
+          this.alert.variant = "warning";
+          this.alert.msg = "Reponse envoyée avec succès";
+          this.alert.dismissCountDown = 5;
         })
         .catch((err) => console.log(err));
     },
