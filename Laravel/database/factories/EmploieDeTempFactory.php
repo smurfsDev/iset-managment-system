70dbdd9c6ebdc283bf10aa587f5fdde45aced70d
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-28 16:07:07 +0100

    [Laravel] creation emploiedetemp factory

---
+++ Laravel/database/factories/EmploieDeTempFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmploieDeTempFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->word(),
+            "classes_id" => $this->faker->randomDigit(),       
+        ];
+    }
+}
