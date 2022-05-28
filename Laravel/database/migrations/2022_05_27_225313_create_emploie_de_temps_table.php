b75ac643eb2df4975cd0e4956315deb7c3ae62df
Mohamed Nour Bensalah <95762228+Moahmednour@users.noreply.github.com>
2022-05-28 16:07:54 +0100

    [Laravel] creation emploiedetemp database

---
+++ Laravel/database/migrations/2022_05_27_225313_create_emploie_de_temps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmploieDeTempsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emploie_de_temps', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('classes_id');
+            $table->foreign('classes_id')->references('id')->on('classes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emploie_de_temps');
+    }
+}
