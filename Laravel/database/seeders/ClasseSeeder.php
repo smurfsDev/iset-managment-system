5a1d7ef1a27f610019e07bc0e03b9c8e26654e2e
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-31 12:08:42 +0100

    [Laravel] seed students and teachers

--- Laravel/database/seeders/ClasseSeeder.php
+++ Laravel/database/seeders/ClasseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Classe;
+use App\Models\Departement;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ClasseSeeder extends Seeder
 {
@@ -116,6 +120,30 @@ class ClasseSeeder extends Seeder
                     ]);
                 }
             }
+        }
+        for ($i = 1; $i <= 25; $i++) {
+            $usr = User::create([
+                'name' => "student" . $i,
+                'email' => "student" . $i . "@example.com",
+                'email_verified_at' => now(),
+                "sexe" => array_rand(["homme", "femme"]),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+            $class = Classe::get()->random();
+            $dep = $class->departement_id;
+            DB::insert('insert into role_user (user_id, role_id,status,classe,department) values (?, ?,?,?,?)', [$usr->id, 2, 1, $class->id,$dep]);
+        }
+        for ($i = 1; $i <= 25; $i++) {
+            $usr = User::create([
+                'name' => "teacher" . $i,
+                'email' => "teacher" . $i . "@example.com",
+                'email_verified_at' => now(),
+                "sexe" => array_rand(["homme", "femme"]),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+            DB::insert('insert into role_user (user_id, role_id,status,department) values (?, ?,?,?)', [$usr->id, 7, 1, Departement::all()->random()->id]);
         }
     }
 
