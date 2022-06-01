08f727abf13272c2a14395c8ec0d5131e115e636
raed charrad <raed.charrad91@gmail.com>
2022-06-01 20:01:10 +0100

    [Laravel] fix routes

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -209,15 +209,6 @@ Route::get("/stats", [DashboardController::class, 'get']);
     });
 
     // documents routes
-Route::group(['prefix' => '/Document'], function () {
-Route::post('/search', [DocumentController::class, 'search']);
-Route::delete('/{id}', [DocumentController::class, 'show']);
-Route::post('/', [DocumentController::class, 'createDocument']);
-Route::put('/{id}', [DocumentController::class, 'updateDocument']);
-Route::delete('/{id}', [DocumentController::class, 'deleteDocument']);
-
-
-    // documents routes
     Route::group(['prefix' => '/Document'], function () {
         Route::post('/search', [DocumentController::class, 'search']);
         Route::delete('/{id}', [DocumentController::class, 'show']);
@@ -343,7 +334,6 @@ Route::group(['prefix' => '/pc'], function () {
     Route::get('/about/{id}', [AboutController::class, 'show']);
     Route::get('/fondator/{id}', [AboutController::class, 'getFondator']);
 });
-
 
 //materiel Controllers
 Route::get('/responsablesMatt', [MaterielController::class, 'getAllResponsableMateriel']);
